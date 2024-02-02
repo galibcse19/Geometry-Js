2c849cf993ef1490f6969e4e9d2212f80e620c83
galibcse19 <mrgalib40@gmail.com>
2024-02-03 01:27:50 +0600

    Backend Targeted work Complete successfully

--- Js/geomatry.js
+++ Js/geomatry.js
@@ -10,14 +10,15 @@ function getDataFromInput(value){
     }
     if(isNaN(inputValueTakeFloat)){
         alert('Please,Enter the Number.')
+        inputValue.value='';
         return;
     }
 
-    inputValue.value='';
+    // inputValue.value='';
     return inputValueTakeFloat;
 }
 
-function setValueToScreen(Id,value){
+function setValueToScreen(Id,value,IdCount){
     const setValue=document.getElementById(Id);
     if(isNaN(value)){
         return;
@@ -26,6 +27,29 @@ function setValueToScreen(Id,value){
         setValue.innerText=value;
     }
 }
+
+// set dataByClicking and Count that
+function setDataInDisplay(AreaType, Area){
+    const SetValueInScreen=document.getElementById('CountSection');
+
+    const count =SetValueInScreen.childElementCount;
+
+    // const SetValueInScreen=document.getElementById('CountSection');
+    // const p=document.createElement('p');
+    // SetValueInScreen.appendChild(p);
+
+    const p=document.createElement('p');
+    p.classList.add('my-2');
+    p.innerHTML= `${count+1}.   ${AreaType}   ${Area}cm<sup>2</sup>  <button class="btn btn-success">Convert</button>`;
+    
+    if(isNaN(Area)){
+        return;
+    }
+    else{
+        SetValueInScreen.appendChild(p);
+    }
+
+}
 // TriangleData
 document.getElementById('TriangleBtn').addEventListener('click',function(){
     const TriangleValue1 = getDataFromInput('TriangleValueB');
@@ -33,7 +57,9 @@ document.getElementById('TriangleBtn').addEventListener('click',function(){
     const FinalArea = 0.5 * TriangleValue1 * TriangleValue2;
      
     setValueToScreen('setTriangleValue',FinalArea);
+    setDataInDisplay('Triangle',FinalArea);
 })
+
 
 // RectangleData
 document.getElementById('RectangleBtn').addEventListener('click',function(){
@@ -42,6 +68,7 @@ document.getElementById('RectangleBtn').addEventListener('click',function(){
     const FinalArea = RectangleValue1 * RectangleValue2;
 
     setValueToScreen('RectangleValue',FinalArea);
+    setDataInDisplay('Rectangle',FinalArea);
 })
 
 // ParallelogramData
@@ -51,6 +78,7 @@ document.getElementById('ParallelogramBtn').addEventListener('click',function(){
     const FinalArea = RectangleValue1 * RectangleValue2;
 
     setValueToScreen('ParallelogramValue',FinalArea);
+    setDataInDisplay('Parallelogram',FinalArea);
 })
 // RhombusData
 document.getElementById('RhombusBtn').addEventListener('click',function(){
@@ -59,6 +87,7 @@ document.getElementById('RhombusBtn').addEventListener('click',function(){
     const FinalArea = RectangleValue1 * RectangleValue2;
 
     setValueToScreen('RhombusValue',FinalArea);
+    setDataInDisplay('Rhombus',FinalArea);
 })
 // PentagonData
 document.getElementById('PentagonBtn').addEventListener('click',function(){
@@ -67,6 +96,7 @@ document.getElementById('PentagonBtn').addEventListener('click',function(){
     const FinalArea = RectangleValue1 * RectangleValue2;
 
     setValueToScreen('PentagonValue',FinalArea);
+    setDataInDisplay('Pentagon',FinalArea);
 })
 // EllipseData
 document.getElementById('EllipseBtn').addEventListener('click',function(){
@@ -75,4 +105,5 @@ document.getElementById('EllipseBtn').addEventListener('click',function(){
     const FinalArea = RectangleValue1 * RectangleValue2;
 
     setValueToScreen('EllipseValue',FinalArea);
+    setDataInDisplay('Ellipse',FinalArea);
 })
